ced2d6cad0da5bc418acf7e9e5a2f8269e521b85
Cris <abdielsolar04@gmail.com>
2026-05-23 15:23:42 -0600

    fix tiene permiso

--- controlador/permisos.php
+++ controlador/permisos.php
@@ -12,6 +12,29 @@ function esEmpleado() {
     return isset($_SESSION['rol']) && ($_SESSION['rol'] == 1 || $_SESSION['rol'] == 2);
 }
 
+function tienePermiso($permiso) {
+    if (!isset($_SESSION['rol'])) {
+        return false;
+    }
+
+    if ($_SESSION['rol'] == 1) {
+        return true;
+    }
+
+    if ($_SESSION['rol'] == 2) {
+        $permitidos = [
+            'caja',
+            'productos',
+            'ventas',
+            'movimientos'
+        ];
+
+        return in_array($permiso, $permitidos);
+    }
+
+    return false;
+}
+
 function soloAdmin() {
     if (!esAdmin()) {
         echo "No tienes permiso";
