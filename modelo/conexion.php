dd4bcf3ce9beb674b55220a1583ce2abc5eed6cd
Cris <abdielsolar04@gmail.com>
2026-05-23 13:59:55 -0600

    sistema listo para render

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,14 +1,24 @@
 <?php
 
-$host = "localhost";
-$usuario = "root";
-$password = "";
-$bd = "inventario";
+$servidor = "sql10.freesqldatabase.com";
+$usuario = "sql10827970";
+$password = "TU_PASSWORD_REAL";
+$base_datos = "sql10827970";
+$puerto = 3306;
 
-$conexion = new mysqli($host, $usuario, $password, $bd);
+$conexion = new mysqli(
+    $servidor,
+    $usuario,
+    $password,
+    $base_datos,
+    $puerto
+);
 
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
+
+$conexion->set_charset("utf8mb4");
+date_default_timezone_set("America/Mexico_City");
 
 ?>
