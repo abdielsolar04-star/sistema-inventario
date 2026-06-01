d01cfe5ce8387f01b28603651ddb5d165b3f6901
Cristian Abdiel Solares Mendez <abdielsolar04@gmail.com>
2026-06-01 01:12:31 -0600

    Corrijo conexion a FreeSQLDatabase

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -2,14 +2,14 @@
 
 $host = "sql10.freesqldatabase.com";
 $usuario = "sql10828040";
-$password = "AQUI_PON_TU_PASSWORD_REAL";
+$password = "AQUI_VA_TU_PASSWORD_REAL";
 $bd = "sql10828040";
 $puerto = 3306;
 
 $conexion = new mysqli($host, $usuario, $password, $bd, $puerto);
 
 if ($conexion->connect_error) {
-    die("Error de conexión a la base de datos: " . $conexion->connect_error);
+    die("Error de conexión BD: " . $conexion->connect_error);
 }
 
 $conexion->set_charset("utf8mb4");
