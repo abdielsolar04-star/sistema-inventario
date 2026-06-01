db5a57a2f25525d18f2f0ab78842845a03bdcc5c
Cristian Abdiel Solares Mendez <abdielsolar04@gmail.com>
2026-06-01 01:03:55 -0600

    Correccion conexion base de datos

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,24 +1,17 @@
 <?php
 
-$servidor = "sql10.freesqldatabase.com";
+$host = "sql10.freesqldatabase.com";
 $usuario = "sql10828040";
-$password = "NrkVnY4tsx";
-$base_datos = "sql10828040";
+$password = "AQUI_PON_TU_PASSWORD_REAL";
+$bd = "sql10828040";
 $puerto = 3306;
 
-$conexion = new mysqli(
-    $servidor,
-    $usuario,
-    $password,
-    $base_datos,
-    $puerto
-);
+$conexion = new mysqli($host, $usuario, $password, $bd, $puerto);
 
 if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    die("Error de conexión a la base de datos: " . $conexion->connect_error);
 }
 
 $conexion->set_charset("utf8mb4");
-date_default_timezone_set("America/Mexico_City");
 
 ?>
