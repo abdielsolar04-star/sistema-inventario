3b77084cd81fd8d72b692d26f884bed5da21b36a
Cristian Abdiel Solares Mendez <abdielsolar04@gmail.com>
2026-06-01 01:19:46 -0600

    Actualizar conexion bd

--- test_db.php
+++ test_db.php
@@ -1,17 +1,17 @@
 <?php
 
 $host = "sql10.freesqldatabase.com";
-$user = "sql10828040";
-$pass = "AQUI_TU_PASSWORD_REAL";
-$db   = "sql10828040";
-$port = 3306;
+$usuario = "sql10828040";
+$password = "PEGA_AQUI_LA_CONTRASEÑA_REAL";
+$bd = "sql10828040";
+$puerto = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conexion = new mysqli($host, $usuario, $password, $bd, $puerto);
 
-if ($conn->connect_error) {
-    die("ERROR BD: " . $conn->connect_error);
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
 }
 
-echo "CONEXIÓN EXITOSA";
+$conexion->set_charset("utf8mb4");
 
 ?>
